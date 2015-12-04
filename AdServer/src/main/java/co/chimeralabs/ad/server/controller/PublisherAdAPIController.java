8986f7b5db2dab1a186e61b7ddb870ed6cbbe805
p12sushilkm <sushilm.iitb@gmail.com>
2015-12-04 18:29:54 +0530

    making changes for uploading to server

--- AdServer/src/main/java/co/chimeralabs/ad/server/controller/PublisherAdAPIController.java
+++ AdServer/src/main/java/co/chimeralabs/ad/server/controller/PublisherAdAPIController.java
@@ -1,21 +1,48 @@
 package co.chimeralabs.ad.server.controller;
 
+import java.io.File;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Random;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import co.chimeralabs.ad.server.apiDTO.AdResourceData;
-import co.chimeralabs.advertiser.client.api.*;;
+import co.chimeralabs.ad.server.apiDTO.ImageTextureAdResponseClientDTO;
+import co.chimeralabs.advertiser.client.api.*;
+import co.chimeralabs.advertiser.client.apiDTO.ImageTextureAdDTO;
+import co.chimeralabs.advertiser.client.apiDTO.ImageTextureAdsDTO;
+import co.chimeralabs.ad.server.util.RetrieveResources;
 
 @RestController
 public class PublisherAdAPIController {
 	
 	@RequestMapping(value={"/publisher/api/loadad", "/"}, method=RequestMethod.GET)
-	public AdResourceData getAdMetadata(){
+	public ImageTextureAdResponseClientDTO getAdMetadata(){
 		AdvertiserServerAPI advertiserServerAPI = new AdvertiserServerAPI();
-		co.chimeralabs.advertiser.client.apiDTO.AdResourceData adData = advertiserServerAPI.getAdResourceData();
-		AdResourceData adResourceData = new AdResourceData(adData.getResourceURL(), adData.getResourceMetadata(), adData.getResourceErrorCode());
+		ImageTextureAdsDTO imageObjects = advertiserServerAPI.getAdResourceData();
+		//co.chimeralabs.advertiser.client.apiDTO.AdResourceData adData = advertiserServerAPI.getAdResourceData();
+		//AdResourceData adResourceData = new AdResourceData(adData.getResourceURL(), adData.getResourceMetadata(), adData.getResourceErrorCode());
 		//AdResourceData adResourceData = new AdResourceData("asdf", "metadata", "200");
-		return adResourceData;
+		ImageTextureAdResponseClientDTO response = new ImageTextureAdResponseClientDTO();
+		if(imageObjects.getErrorMsg() != null){
+			response.setResourceErrorCode(0);
+			response.setErrorMsg("");
+		}
+		else{
+			response.setResourceErrorCode(1);
+			response.setErrorMsg(imageObjects.getErrorMsg());
+		}
+		List<ImageTextureAdDTO> ads = imageObjects.getAds();
+		int size = ads.size();
+		Random random = new Random();
+		int index = random.nextInt(size);
+		ImageTextureAdDTO imageObject = ads.get(index);
+		InputStream inputStream = getClass().getResourceAsStream("/machine/MachineConstants.xml");
+		String baseDir = RetrieveResources.retrieveResourcesAppConatants(inputStream, "imageresourceurl").get(0);
+		String url = baseDir + File.separator + imageObject.getAdResourceIdentifier() +"."+imageObject.getAdResourceFormat();
+		response.setResourceURL(url);
+		return response;
 	}
 }
